7eee3aa5d15649488fc8104d69284176121d669f
Bob Saris <bob@bobsaris.com>
2017-07-30 15:25:44 -0400

    Updated record to record in 10 minute intervals (to make testing easier) as well as switching to sox to record.

--- record.sh
+++ record.sh
@@ -9,12 +9,12 @@ exec 1> >( tee -a $LOGFILE ) 2>&1
 echo Starting at $(date)
 
 # record in hourly chunks
-SEC=`expr 60 \* 60`
+SEC=`expr 60 \* 10`
 echo Creating files every $SEC
 
 # start recording
-# sox -d bubbles_.wav channels 1 rate 48k trim 0 $SEC : newfile : restart
-arecord -v -D plughw:CARD=CODEC,DEV=0 -f dat -t wav -c 1 --max-file-time=$SEC --use-strftime ./wav/%Y-%m-%d-%H-%M-%S.wav
+AUDIODEV=plughw:CARD=CODEC,DEV=0 sox -d ./wav/bubbles_.wav channels 1 rate 48k trim 0 $SEC : newfile : restart
+# arecord -v -D plughw:CARD=CODEC,DEV=0 -f dat -t wav -c 1 --max-file-time=$SEC --use-strftime ./wav/%Y-%m-%d-%H-%M-%S.wav
 
 echo Done
 
